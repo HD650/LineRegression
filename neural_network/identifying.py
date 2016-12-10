2d6303b8adf8edcbb5f17068a7398950befb577f
HD650 <unrealcat@outlook.com>
2016-12-10 21:39:32 +0800

    some fix

--- neural_network/identifying.py
+++ neural_network/identifying.py
@@ -5,7 +5,7 @@ if __name__ == '__main__':
     print('start deduction...')
     print('please enter the hand writing directory(absolute directory:)')
     directory = input()
-    directory = './data/your_test_set/{0}.bmp'.format(directory)
+    directory = '{0}'.format(directory)
     print('please enter the desired output:')
     desired_output = int(input())
     test_from_image(net, directory, desired_output)
